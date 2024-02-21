99ea973341aa92563c81a212962b0ef8f99dfe08
Cupid Valentine <westerngod@126.com>
2024-02-21 22:03:52 +0800

    chore: remove unrelated codes

--- apps/frontend/src/Register.tsx
+++ apps/frontend/src/Register.tsx
@@ -1,5 +1,5 @@
 import { Button, Form, Input, message } from 'antd'
-import { login, register, registerCaptcha } from './interfaces'
+import {  register, registerCaptcha } from './interfaces'
 import { useForm } from 'antd/es/form/Form'
 import { useCallback } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
@@ -11,25 +11,6 @@ export interface RegisterUser {
   confirmPassword: string
   email: string
   captcha: string
-}
-
-const onFinish = async (values: RegisterUser) => {
-  const res = await login(values.username, values.password)
-
-  const { code, message: msg, data } = res.data
-  if (res.status === 201 || res.status === 200) {
-    message.success('登录成功')
-
-    localStorage.setItem('access_token', data.accessToken)
-    localStorage.setItem('refresh_token', data.refreshToken)
-    localStorage.setItem('user_info', JSON.stringify(data.userInfo))
-
-    setTimeout(() => {
-      navigate('/')
-    }, 1000)
-  } else {
-    message.error(data || '系统繁忙，请稍后再试')
-  }
 }
 
 const layout1 = {
